4730a92427b5eaa41ec4f66da412f35d9ad10d63
Scenaristeur <scenaristeur@gmail.com>
2022-06-21 10:16:42 +0200

    Initial dist subtree commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+const publicPath = process.env.NODE_ENV === 'production' ? '/solid-group/' : '/'
+
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+   publicPath: publicPath,
   transpileDependencies: true
 })
