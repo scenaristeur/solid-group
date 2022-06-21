3323a05dff51656cc1a015f308048ae3101fc3e0
Scenaristeur <scenaristeur@gmail.com>
2022-06-21 10:16:42 +0200

    Initial dist subtree commit

---
+++ js/about-legacy.6540c6c4.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksolid_group"]=self["webpackChunksolid_group"]||[]).push([[443],{5830:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],l=n(1001),r={},a=(0,l.Z)(r,u,s,!1,null,null,null),i=a.exports}}]);
+//# sourceMappingURL=about-legacy.6540c6c4.js.map
